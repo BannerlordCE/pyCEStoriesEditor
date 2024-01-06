dae5270ddd6bb53db22ec4dcda52aa568560400a
bicobus <bicobus@keemail.me>
2024-01-06 08:02:09 +0100

    Move build data to setup.py file

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 import sys
 from cx_Freeze import setup, Executable
 
-base = 'Win32GUI' if sys.platform=='win32' else None
+base = 'Win32GUI' if sys.platform == 'win32' else None
 
 executables = [
-    Executable('src/pycestorieseditor/__main__.py', base=base, target_name = 'pce')
+    Executable('src/pycestorieseditor/__main__.py', base=base, target_name='pce')
 ]
+buildexe = {
+    'excludes': [
+        "tkinter", "gi", "gtk", "PyQt4", "PyQt5", "PyQt6",
+        "PySide2", "PySide6", "shiboken2", "shiboken6", "lib2to3"
+    ],
+    'packages': ["xsdata_attrs.hooks"],
+    'include_msvcr': False
+}
 
-setup(executables = executables)
+setup(
+    executables=executables,
+    options={
+        "build_exe": buildexe
+    }
+)
