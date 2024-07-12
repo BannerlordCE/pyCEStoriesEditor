f0f021b2dc9a01d6abee855d07343ae2fac3fa9f
bicobus <bicobus@keemail.me>
2024-07-12 22:16:13 +0200

    genfromxsd: Apply black on newly generated template file

--- genfromxsd.sh
+++ genfromxsd.sh
@@ -6,5 +6,9 @@ sed -i -e 's/import attr/from attrs import define, field, validators/g' \
     -e 's/@attr\.s/@define/g' \
     -e 's/attr\.ib/field/g' ./generated/ceevents_modal.py
 
+if command -v black > /dev/null; then
+    black generated/ceevents_modal.py
+fi
+
 echo "Please merge ./generated/ceevents_modal.py with ./src/pycestorieseditor/ceevents_template/ceevents_modal.py"
 echo "Be careful to not remove added code."
